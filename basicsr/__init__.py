a61c4e4e4d819e374de678f6296bc5896b944ff1
Paris Holley <paris@mantisadnetwork.com>
2026-02-01 19:51:29 -0600

    fix

--- basicsr/__init__.py
+++ basicsr/__init__.py
@@ -1,12 +1,10 @@
 # https://github.com/xinntao/BasicSR
 # flake8: noqa
-from .archs import *
-from .data import *
-from .losses import *
-from .metrics import *
-from .models import *
-from .ops import *
-from .test import *
-from .train import *
-from .utils import *
+import sys as _sys
+
+# Ensure absolute imports like `import basicsr` work when bundled as a custom node.
+_sys.modules.setdefault("basicsr", _sys.modules[__name__])
+
 from .version import __gitsha__, __version__
+
+__all__ = ["__gitsha__", "__version__"]
